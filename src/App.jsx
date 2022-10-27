3dd4e24eb1d845117bd5c2c4447856dcd154cd19
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-10-27 17:30:12 +0530

    feat: added complete dashboard ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import { theme } from './theme'
-import DashBoard from './pages/DashBoard';
-import Main from './pages/Index';
 import Error from './pages/Error';
 import Login from './pages/Login';
 import Landing from './pages/Index';
 import Wizard   from "./pages/Wizard"
+import DashBoard from "./pages/DashBoard";
 
-import SideSection from './pages/dashboard/SideSection';
+
 
 function App() {
   return (
@@ -19,8 +18,6 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/dashboard" element={<DashBoard />} />
       <Route path="/login" element={<Login />} />
-
-      <Route path="/sidedash" element={<SideSection/>}></Route>
       <Route path="/wizard" element={<Wizard />} />
       <Route path="*" element={<Error />} />
     </Routes>

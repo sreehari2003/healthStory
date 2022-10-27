a4c5d9bb69ac70085cc3baed4674a5e09adb1358
Asif Muhammed <asifasifpsps@gmail.com>
2022-10-27 22:42:44 +0530

    fix : Some minor  changes made

--- src/pages/Wizard.jsx
+++ src/pages/Wizard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Center, FormControl, FormHelperText, FormLabel, Heading, Input, Button, Alert,Text, FormErrorMessage } from "@chakra-ui/react";
+import { Box, Center, FormControl, FormHelperText, FormLabel, Heading, Input, Button, Alert, Text, FormErrorMessage } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { Navbar } from "../Layout/components";
 import { Doctor } from "../components/modal";
@@ -16,22 +16,23 @@ const Wizard = () => {
   }
   console.log(errors)
   return (
-    <Box p="40px" minH="100vh" >
+    <Box p="30px" minH="100vh" >
+
       <Navbar />
 
 
-      <Box mt="40px"  >
+      <Box mt="40px" bg={'#f8f9fa'} width='35%' borderRadius={'lg'} height='500px' ml='30%'>
         <Center >
-          <Heading size="md" borderRadius={'md'} fontWeight="500" width='500px' height='50px' pt={2} >
+          <Heading size="md" borderRadius={'md'} fontWeight="700" width='500px' height='50px' pt={8} >
             <Center>
-            <Text fontSize='2xl'>DOCTORS INFORMATION</Text>
-              
+              <Text fontSize='2xl'>DOCTORS INFORMATION</Text>
+
             </Center>
           </Heading>
         </Center>
         <Center flexDir="column" pt={5}>
 
-          <Box width='40%' >
+          <Box width='80%' >
             <form onSubmit={handleSubmit(onSubmit)}>
               <FormControl mt="20px" isInvalid={errors.location}>
                 <FormLabel>Location</FormLabel>
@@ -47,8 +48,8 @@ const Wizard = () => {
                 <Input type="text" {...register("mobnum", { required: true, pattern: /^[0-9+-]+$/, minLength: 6, maxLength: 12 })} />
                 {errors.mobnum && (
 
-<FormErrorMessage>Mobile Number is required.</FormErrorMessage>
-)}
+                  <FormErrorMessage>Mobile Number is Invalid.</FormErrorMessage>
+                )}
               </FormControl>
               <FormControl mt="20px" isInvalid={errors.qualification}>
                 <FormLabel>Education Qualification</FormLabel>

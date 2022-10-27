afec4d6374e30e80f348bc6e94108f94867d7cfd
reenphygeorge <reenphygeorge@gmail.com>
2022-10-28 00:35:53 +0530

    chore: dotenv setup

--- server/index.js
+++ server/index.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client')
+require('dotenv').config()
 
 const prisma = new PrismaClient()
 const app = express()
 
 app.get('/', async (req, res) => {
     res.send("Hello World");
-})
-
-app.post('/post', async (req, res) => {
-    const { title, content, authorEmail } = req.body
-    const post = await prisma.post.create({
-        data: {
-            title,
-            content,
-            published: false,
-            author: { connect: { email: authorEmail } },
-        },
-    })
-    res.json(post)
 })
 
 const server = app.listen(3000)

28a1678ea7df9a009d6ec47bfa657be82126d5e9
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-10-28 04:29:15 +0530

    fix: cors

--- server/index.js
+++ server/index.js
@@ -1,79 +1,122 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client')
-require('dotenv').config()
+const express = require("express");
+var cors = require("cors");
+const app = express();
+app.use(cors());
 
-const app = express()
-app.use(express.json({ extended: true }))
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
 
-const prisma = new PrismaClient()
+app.use(express.json({ extended: true }));
+
+const prisma = new PrismaClient();
 async function main() {
-    // Connect the client
-    await prisma.$connect()
-    // ... you will write your Prisma Client queries here
+  // Connect the client
+  await prisma.$connect();
+  // ... you will write your Prisma Client queries here
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
-
-app.get('/', async (req, res) => {
-    res.send("Hello World");
-})
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
 
 // Doctor Routes //
-app.get('/doctor/:uid', async (req, res) => {
-    const posts = await prisma.doctor.findUnique({
-        where: { uid: req.params.uid }
-    })
-    res.send(posts)
-})
 
-app.post('/doctor', async (req, res) => {
-    const user = await prisma.doctor.create({
-        data: {
-            "name": req.body.name,
-            "email": req.body.email,
-            "photo": req.body.photo,
-            "uid": req.body.uid,
-            "qualification": req.body.qualification,
-            "passyear": req.body.passyear,
-            "College": req.body.College
-        },
-    })
-    res.send(user)
-})
+app.post("/doctor", async (req, res) => {
+  const exist = await prisma.doctor.findUnique({
+    where: {
+      uid: req.body.uid,
+    },
+  });
+  if (exist) {
+    return res.json({
+      ok: true,
+      message: "welcome back",
+      data: exist,
+    });
+  }
+  const user = await prisma.doctor.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      photo: req.body.photo,
+      uid: req.body.uid,
+      qualification: [],
+      passyear: req.body.passyear,
+      College: req.body.College,
+    },
+  });
+  res.json({
+    ok: true,
+    message: "account was created",
+    data: user,
+  });
+});
 
-app.get('/user/:id', async (req, res) => {
-    const user = await prisma.user.findUnique({
-      where: { uid : req.params.id },
-    })
-    res.json(user)
-  })
+app.get("/doctor/:uid", async (req, res) => {
+  const posts = await prisma.doctor.findUnique({
+    where: { uid: req.params.uid },
+  });
+  res.json({
+    ok: true,
+    message: "query succesfull",
+    data: posts,
+  });
+});
 
-  
+app.post("/user", async (req, res) => {
+  console.log("user calling");
+  const { name, email, photo, uid } = req.body;
+  try {
+    const exist = await prisma.user.findUnique({
+      where: {
+        uid: req.body.uid,
+      },
+    });
+    if (exist) {
+      return res.json({
+        ok: true,
+        message: "welcome back",
+        data: exist,
+      });
+    }
 
-  app.post('/user', async (req, res) => {
-    const { name, email, photo, uid  } = req.body
     const user = await prisma.user.create({
       data: {
         name,
         email,
         photo,
         uid,
-        data: {}
+        data: {},
       },
-    })
-    res.json(user)
-  })
+    });
+    res.json({
+      ok: true,
+      message: "account was created",
+      data: user,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
+app.get("/user/:id", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { uid: req.params.id },
+  });
+  res.json({
+    ok: true,
+    message: "account was created",
+    data: user,
+  });
+});
 
-
-  
-
-const server = app.listen(3000)
+const server = app.listen(3000);

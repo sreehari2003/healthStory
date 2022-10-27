ef4f3843fc7b0e18ec8eb9bfa368a74b8e370cf9
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-10-27 18:26:28 +0530

    fix: supabase

--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useState,useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 export const AuthCTX = React.createContext(null);
@@ -9,14 +9,28 @@ const supabase = createClient(
 );
 
 const AuthContext = ({ children }) => {
+    const [user,setUser ] = useState([]);
+   console.log(user);
   async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
     });
+    console.log(data);
   }
   async function signout() {
     const { error } = await supabase.auth.signOut();
   }
+
+  useEffect(()=>{
+    (async()=>{
+        const { data } = await supabase
+        .from('users')
+        .select('id, username, avatar_url, website')
+        setUser(data);
+        console.log(data);
+    })()
+ 
+  },[])
 
   const values = {
     signInWithGoogle,
